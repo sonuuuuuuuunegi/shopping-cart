17c721d022e26d4cfdebd4bbac69910732c83132
sonuuuuuuuunegi <sachinnegi220@gmail.com>
2024-09-02 10:07:59 +0530

    implemented category services

--- src/main/java/com/sonu/shoppingCart/service/category/CategoryService.java
+++ src/main/java/com/sonu/shoppingCart/service/category/CategoryService.java
@@ -1,4 +1,54 @@
 package com.sonu.shoppingCart.service.category;
 
-public class CategoryService {
+import com.sonu.shoppingCart.exception.AlreadyExistsException;
+import com.sonu.shoppingCart.exception.ResourceNotFoundException;
+import com.sonu.shoppingCart.model.Category;
+import com.sonu.shoppingCart.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService implements ICategoryService{
+    private final CategoryRepository categoryRepository;
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("category not found"));
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return Optional.of(category).filter(c->!categoryRepository.existsByName(c.getName()))
+                .map(categoryRepository::save)
+                .orElseThrow(()->new AlreadyExistsException(category.getName()+" Already Exists"));
+    }
+
+    @Override
+    public Category updateCategory(Category category,Long id) {
+        return Optional.ofNullable(getCategoryById(id)).map(oldCategory->{
+            oldCategory.setName(category.getName());
+            return categoryRepository.save(oldCategory);
+        }).orElseThrow(()->new ResourceNotFoundException("category not found!!"));
+    }
+
+    @Override
+    public void deleteCategoryById(Long id) {
+        categoryRepository.findById(id)
+                .ifPresentOrElse(categoryRepository::delete,
+                        ()->{throw new ResourceNotFoundException("Category not found");});
+    }
 }
--- src/main/java/com/sonu/shoppingCart/service/image/IImageService.java
+++ src/main/java/com/sonu/shoppingCart/service/image/IImageService.java
@@ -1,4 +1,5 @@
 package com.sonu.shoppingCart.service.image;
 
 public interface IImageService {
+
 }
